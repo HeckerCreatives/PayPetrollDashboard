2a83b3eab410978969ec1d556e2fc7d7e4ea9966
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-08-16 15:38:15 +0800

    pooosh

--- src/app/superadmin/playeraccount/GameHistory.tsx
+++ src/app/superadmin/playeraccount/GameHistory.tsx
@@ -78,7 +78,7 @@ export default function GameHistory() {
         }, 500); 
     
         return () => clearTimeout(delayDebounceFn); 
-      }, [refresh]);
+      }, [refresh, currentpage]);
 
     const handlePageChange = (page: number) => {
         setCurrentPage(page)
