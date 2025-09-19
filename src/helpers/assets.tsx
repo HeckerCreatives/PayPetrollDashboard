371c055fa82921bcd523d063b99b10246884f376
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-09-19 09:01:12 +0800

    new nfts

--- src/helpers/assets.tsx
+++ src/helpers/assets.tsx
@@ -1,44 +1,35 @@
 export const nftImage = (data: string) => {
-    if(data.toLowerCase() === 'iron puppy'){
-        return '/nft/ironpuppy.jpg'
-    }else if(data === 'Shiba Hulk'){
-        return '/nft/shibahulk.png'
-    }else if(data === 'Captain Hachi'){
-        return '/nft/captainhachi.jpg'
-    }else if(data === 'Thor Inu'){
-        return '/nft/thorinu.jpg'
-    }else if (data === 'Shibathanos') {
-        return '/nft/shibathanos.jpg'
-    }else if (data === 'Spider Puppy') {
-        return '/nft/spider_puppy.png'
-    }else if (data === 'Black Hachi') {
-        return '/nft/black_hachi.png'
-    }else if (data === 'Shiba Widow') {
-        return '/nft/shiba_widow.png'
-    }else if (data === 'Doctor Puppy') {
-        return '/nft/doctor_puppy.png'
-    }else if (data === 'Captain Inu') {
-        return '/nft/captain_inu.png'
-    }else if (data === 'DOG POOL') {
-        return '/nft/dogpool.jpg'
-    }else if (data === 'Hachi Fury') {
-        return '/nft/hachifury.jpg'
-    }else if (data === 'Shibarine') {
-        return '/nft/shibarine.jpg'
-    }else if (data === 'Magnedog') {
-        return '/nft/magnedog.jpg'
-    }else if (data === 'Shibaclops') {
-        return '/nft/shibaclops.jpg'
-    }else if (data === 'ANT DOG') {
-        return '/nft/antdog.png'
-    }else if (data === 'LOCHI') {
-        return '/nft/lochi.png'
-    }else if (data === 'Hachi Eye') {
-        return '/nft/hachieye.png'
-    }else if (data === 'Droog') {
-        return '/nft/droog.png'
-    }else if (data === 'Dogmora') {
-        return '/nft/dogmora.png'
-    }
-    
+  console.log(data)
+  const nftMap: Record<string, string> = {
+    "iron puppy": "/nft/ironpuppy.jpg",
+    "shiba hulk": "/nft/shibahulk.png",
+    "captain hachi": "/nft/captainhachi.jpg",
+    "thor inu": "/nft/thorinu.jpg",
+    "shibathanos": "/nft/shibathanos.jpg",
+    "spider puppy": "/nft/spider_puppy.png",
+    "black hachi": "/nft/black_hachi.png",
+    "shiba widow": "/nft/shiba_widow.png",
+    "doctor puppy": "/nft/doctor_puppy.png",
+    "captain inu": "/nft/captain_inu.png",
+    "dog pool": "/nft/dogpool.jpg",
+    "hachi fury": "/nft/hachifury.jpg",
+    "shibarine": "/nft/shibarine.jpg",
+    "magnedog": "/nft/magnedog.jpg",
+    "shibaclops": "/nft/shibaclops.jpg",
+    "ant dog": "/nft/antdog.png",
+    "lochi": "/nft/lochi.png",
+    "hachi eye": "/nft/hachieye.png",
+    "droog": "/nft/droog.png",
+    "dogmora": "/nft/dogmora.png",
+
+    "winter doggie": "/nft/winterdoggie.png",
+    "dog machine": "/nft/dogmachine.png",
+    "waspinu": "/nft/waspinu.png",
+    "scarlet dog": "/nft/scarletdog.png",
+    "quick doggie": "/nft/quickdoggie.png",
+  }
+
+  const key = data.toLowerCase()
+
+  return nftMap[key] || undefined
 }
